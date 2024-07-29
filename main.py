f78a3f0399792c2fafed5322defd1c7b5ca5d9db
wa7eed <ahmedelbanna908@gmail.com>
2024-07-29 17:14:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+
+# Create a turtle object
+t = turtle.Turtle()
+
+# Set the turtle's speed to a slow speed
+t.speed(0)
+
+# Use the turtle's pen to draw the letters of "abdallah"
+
+t.goto(-100, 0)
+t.pendown()
+t.write("fuck you omar", font=("Arial", 20, "bold"))
+
+# Close the turtle window
+turtle.done()
